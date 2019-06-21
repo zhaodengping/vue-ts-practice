37dcb884c1cfeedc66f82e1ae41c0d8a0f25ca2d
liuwei <andyliu0905@163.com>
2019-06-21 14:58:10 +0800

    临时客户管理

--- src/components/search/clientSource.vue
+++ src/components/search/clientSource.vue
@@ -1,20 +1,39 @@
 
 <template>
     <div>
-        <el-select v-model="select">
-          <el-option v-for="(item,index) in clientList" :key="index"></el-option>
+        <el-select v-model="select" @change="changeSource">
+          <el-option v-for="(item,index) in clientList" :key="index" :value="item.value" :label="item.label"></el-option>
         </el-select>
     </div>
 </template>
 <script lang="ts">
 // 客户来源
 import Vue from 'vue'
-let clientList:number[]=["a","b","c"]
-export default Vue.extend({
-    data(){
-        return{
-            select:[]
+import {Component,Prop,Model,Emit} from 'vue-property-decorator';
+@Component 
+export default class ClientSource extends Vue{
+    private clientList:object[]=[
+        {
+          value: '选项1',
+          label: '黄金糕'
+        }, {
+          value: '选项2',
+          label: '双皮奶'
+        }, {
+          value: '选项3',
+          label: '蚵仔煎'
+        }, {
+          value: '选项4',
+          label: '龙须面'
+        }, {
+          value: '选项5',
+          label: '北京烤鸭'
         }
+    ]
+    private select:string=""
+    @Emit("changeSourceData")
+    changeSource(e:string):void{
+        this.select=e
     }
-})
+}
 </script>
---
+++ src/components/search/handleStatus.vue
@@ -0,0 +1,37 @@
+<template>
+    <!-- 处理状态 -->
+    <div>
+        <el-select v-model="status" @change="changeStatus">
+          <el-option v-for="(item,index) in statusList" :key="index" :value="item.value" :label="item.label"></el-option>
+        </el-select>
+    </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import {Component,Emit} from 'vue-property-decorator';
+@Component
+export default class HandleStatus extends Vue{
+    statusList:object[]=[{
+          value: '选项1',
+          label: '黄金糕'
+        }, {
+          value: '选项2',
+          label: '双皮奶'
+        }, {
+          value: '选项3',
+          label: '蚵仔煎'
+        }, {
+          value: '选项4',
+          label: '龙须面'
+        },{
+          value: '选项5',
+          label: '北京烤鸭'
+        }];
+    status:string="";
+
+    @Emit("changeStatusData")
+    changeStatus(val:string):void{
+        this.status=val
+    }
+}
+</script>
