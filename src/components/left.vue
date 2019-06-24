a72be55d683fc1b85cc2468b79e7cb6f3c949a23
liuwei <andyliu0905@163.com>
2019-06-24 18:38:28 +0800

    代理商详情

--- src/components/left.vue
+++ src/components/left.vue
@@ -3,7 +3,7 @@
     <el-menu router>
       <el-submenu index="1">
         <template slot="title">
-          <i class="el-icon-location"></i>
+          <i class="iconfont iconhome"></i>
           <span>代理商列表</span>
         </template>
         <el-menu-item index="/temAccount">临时客户管理</el-menu-item>
@@ -13,7 +13,7 @@
 
       <el-submenu index="2">
         <template slot="title">
-          <i class="el-icon-location"></i>
+          <i class="iconfont iconchanpin"></i>
           <span>产品管理</span>
         </template>
         <el-menu-item index="/record">产品档案</el-menu-item>
@@ -32,6 +32,9 @@
     background-color: #273142;
     .el-submenu__title {
       color: var(--theme-color);
+      i{
+        margin-right: 10px;
+      }
       &:hover {
         background-color: #273142;
       }
